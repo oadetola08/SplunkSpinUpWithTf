763b047a9b4fabe2d4633afbe822201f27edd528
oadetola08 <61671087+oadetola08@users.noreply.github.com>
2023-10-02 16:24:32 -0500

    Create new user and create index

--- main.tf
+++ main.tf
@@ -3,6 +3,29 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "random_string" "random_username" {
+  length  = 8
+  special = false
+}
+
+resource "random_string" "password" {
+  length           = 16
+  special          = true
+  override_special = "!@#*"
+}
+
+output "generated_IP" {
+  value = aws_instance.splunk_instance.public_ip
+}
+
+output "generated_username" {
+  value = random_string.random_username.result
+}
+
+output "generated_password" {
+  value = random_string.password.result
+}
+
 resource "aws_security_group" "tola004_sg" {
   name        = "tola004-security-group"
   description = "An tola004 security group for EC2 instances"
@@ -47,6 +70,7 @@ resource "aws_security_group" "tola004_sg" {
   }
 }
 
+
 resource "aws_instance" "splunk_instance" {
 ami="ami-0e40787bcd2f11bbb"
   instance_type = "t2.micro"
@@ -55,9 +79,10 @@ tags = {
     Name = "SplunkInstance"
   }
 vpc_security_group_ids = [aws_security_group.tola004_sg.id]
+
 # Provisioner to wait until the instance is accessible over the internet
 provisioner "local-exec" {
-  # Wait for up to 5 minutes (60 seconds) for SSH to become available
+  # Wait for up to 5 minutes for SSH to become available
   when    =create  # Run this provisioner during resource creation
   command = <<-EOT
     while ! nc -z -w 5 ${aws_instance.splunk_instance.public_ip} 8089; do
@@ -65,8 +90,23 @@ provisioner "local-exec" {
       sleep 5
     done
     echo "SSH is now accessible. Proceeding with the command."
-    python3 my_script.py ${aws_instance.splunk_instance.id} ${aws_instance.splunk_instance.public_ip}
-  EOT
-}
-}
+    curl -k -u admin:SPLUNK-${aws_instance.splunk_instance.id} \
+        https://${aws_instance.splunk_instance.public_ip}:8089/services/authentication/users -d \
+        "name=${random_string.random_username.result}&password=${random_string.password.result}&roles=user"
+    
+# Create indexs
+    echo "Now creating indexs"
+    curl -k -u admin:SPLUNK-${aws_instance.splunk_instance.id} \
+      "https://${aws_instance.splunk_instance.public_ip}:8089/services/data/indexes" \
+      -d "name=my_custom_index" \
+      -d "datatype=event" \
+      -d "maxTotalDataSizeMB=50000" \
+      -d "homePath=\$SPLUNK_DB/main/db" \
+      -d "coldPath=\$SPLUNK_DB/main/colddb" \
+      -d "thawedPath=\$SPLUNK_DB/main/thaweddb"
+      
+    echo "Indexs completed"
 
+  EOT
+}
+}
